c6591fbe0d8f575dc892f3f438b7bbc9e42dcaee
WestringLiu <1364643962@qq.com>
2018-02-25 09:54:31 +0800

    add dir chapter[1-5]

---
+++ chapter[1-5]/kw.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def person(name,age,**kw):
+    print('name:',name,'age:',age,'other:',kw)
---
+++ chapter[1-5]/person.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def person(name,age,*,city,job):
+    print(name,age,city,job)
